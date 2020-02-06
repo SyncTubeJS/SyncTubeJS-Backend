10022d147a0b37e3eaaf6b499624f010659bb806
Joseph Richey <josephrichey@me.com>
2020-02-06 12:27:20 -0500

    Cleaned up code and also added new_message action

--- app.js
+++ app.js
@@ -7,39 +7,34 @@ console.log("Server Started.");
 io.on('connection', function (socket) {
   socket.emit('console_message', { status: 'Connected! Hello World!' });
 
-  socket.on('test_connect', function (data) {
-    console.log(data);
-    console.log(socket.id)
+  // Test Code
+  // socket.on('test_connect', function (data) {
+  //   console.log(data);
+  //   console.log(socket.id)
 
-    socket.emit('console_message', { status: 'Payload Received Properly.' });
-  });
+  //   socket.emit('console_message', { status: 'Payload Received Properly.' });
+  // });
 
-
-  socket.on('create_room', function(room_name){
+  // Logic that runs when a user joins an existing room or creates a new room
+  socket.on('join_create_room', function(room_name){
     if(room_name.length > 0) {
       socket.join(room_name)
     } else {
       console.log('nope!')
     }
-    
-    // @joe Should we completely change store with all Rooms instead of pushing just one room at a time? I have both below
-
-
-    let allRooms = Object.keys(io.sockets.adapter.rooms)
-    console.log(allRooms)
 
     // ONE AT A TIME This runs the Vuex action SOCKET_addRoomAction
     socket.emit('addRoomAction', room_name)
-
-    // COMPLETELY CHANGE STATE
-    // socket.emit('changeRoomAction', allRooms)
   })
 
+  // "Logic" for chat function
+  socket.on('new_message', function(message){
+    socket.emit('addNewMessage', message)
+  });
 
+  //  -- TODO: CREATE SOCKET THAT ACCEPTS A YOUTUBE URL
+
+  //  -- TODO: CREATE A SOCKET THAT EMITS ABOVE YOUTUBE URL TO PEERS IN THE NAMESPACE/LOBBY
+
+  // -- TODO: CREATE A SOCKET THAT SYNCS TIME UP WHEN THE HOST SKIPS AROUND IN A VIDEO
 });
-
-//  -- TODO: CREATE SOCKET THAT ACCEPTS A YOUTUBE URL
-
-//  -- TODO: CREATE A SOCKET THAT EMITS ABOVE YOUTUBE URL TO PEERS IN THE NAMESPACE/LOBBY
-
-// -- TODO: CREATE A SOCKET THAT SYNCS TIME UP WHEN THE HOST SKIPS AROUND IN A VIDEO
